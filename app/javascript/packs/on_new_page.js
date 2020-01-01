4cd0d099a9e6fa92b01b39688e4049220508fa81
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2020-01-01 11:25:14 +0000

    getting further along

--- app/javascript/packs/on_new_page.js
+++ app/javascript/packs/on_new_page.js
@@ -1,8 +1,9 @@
 import { editSwitch } from "../components/edit_switch"
+import { updateSpkrOnEdit } from "../components/update_spkr_from_tlk"
 
 let editMode = document.querySelector('#meta').content
 
 if(editMode == 'tlks-show-edit') {
   editSwitch();
-  console.log('edit mode present!')
+  updateSpkrOnEdit();
 }
