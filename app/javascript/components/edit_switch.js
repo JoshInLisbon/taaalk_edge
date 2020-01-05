c0202e9872efdeee3077e4fcd251b7b9be1a35c7
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2020-01-05 01:09:08 +0000

    nearly finished

--- app/javascript/components/edit_switch.js
+++ app/javascript/components/edit_switch.js
@@ -21,11 +21,3 @@ const changeBtnText = () => {
     editBtn.innerText = "Edit This Taalk";
   }
 }
-
-// const toggleZone = (zone) => {
-//   zone.classList.toggle('tlk-section-on');
-//   zone.classList.toggle('tlk-section-off');
-// }
-
-// update buttons need save
-
