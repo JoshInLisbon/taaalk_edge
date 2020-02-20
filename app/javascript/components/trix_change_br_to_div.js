5da933ad6135ccf97b5efe15e3ff15fea21e7346
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2020-02-20 14:43:05 +0000

    solve trix editor <br> issues inc mobile and image edge cases

--- app/javascript/components/trix_change_br_to_div.js
+++ app/javascript/components/trix_change_br_to_div.js
@@ -1,69 +1,3 @@
-// const changeBrToDiv = () => {
-//   let msgTrixInputDivs = document.querySelectorAll(".spkr-msg-form");
-//   msgTrixInputDivs.forEach(div => {
-//     let msgTrixInput = div.querySelector("trix-editor");
-//     let msgTrixStore = div.querySelector('[name="msg[content]"]');
-//     let tlkBubbleStart = '<div class="tlk-bubble">';
-//     let tlkBubbleEnd = '</div>';
-
-//     let elemTarget = msgTrixInput.getAttribute('target');
-//     // $(`[target="${elemTarget}"]`).val('');
-//     $(`[target="${elemTarget}"]`).bind('input keyup', function(){
-
-//       // pasted
-//       let textBlock = []
-
-//       let brMatchesBr = msgTrixStore.value.match(/(?<=<br>)([\s\S]*?)(?=<br>)/g);
-//       let brMatchDiv = msgTrixStore.value.match(/<br>(?![\s\S]*<br>[\s\S]*$)([\s\S]*?)(?:<\/div>)/);
-//       let blockMatchBr = msgTrixStore.value.match(/(?:<!--block-->)([\s\S]*?)(?=<br>)/);
-//       let blockMatchDiv = msgTrixStore.value.match(/(?:<!--block-->)([\s\S]*?)(?=<\/div>)/);
-
-//       // if(!blockMatchDiv || !blockMatchBr) {
-//       //   textBlock.push(tlkBubbleStart, msgTrixInput.value, tlkBubbleEnd);
-//       //   msgTrixStore.value = textBlock.join("");
-//       // }
-//       if(brMatchesBr) {
-//         textBlock.push(tlkBubbleStart, blockMatchBr[1], tlkBubbleEnd);
-//         brMatchesBr.forEach(match => {
-//           if(match) {
-//             textBlock.push(tlkBubbleStart, match, tlkBubbleEnd);
-//           }
-//         });
-//         if(brMatchDiv[1]) {
-//           textBlock.push(tlkBubbleStart, brMatchDiv[1], tlkBubbleEnd);
-//         }
-//       } else if(blockMatchBr) {
-//         textBlock.push(tlkBubbleStart, blockMatchBr[1], tlkBubbleEnd);
-//         if(brMatchDiv[1]) {
-//           textBlock.push(tlkBubbleStart, brMatchDiv[1], tlkBubbleEnd);
-//         }
-//       } else if (blockMatchDiv) {
-//         // alert(msgTrixInput.value);
-//         textBlock.push(tlkBubbleStart, blockMatchDiv[1], tlkBubbleEnd);
-//       }
-//       // console.log(textBlock.join(""))
-//       // msgTrixStore.value = textBlock.join("");
-
-//       // let bubbles = document.querySelectorAll('.tlk-bubble')
-//       // bubbles.forEach(bubble => {
-//       //   bubble.innerText = msgTrixStore.value;
-//       //   console.log(msgTrixStore.value);
-//       // });
-//     });
-
-//     // msgTrixInput.addEventListener('keyup', (event) => {
-//     //   // cut
-
-//     // });
-
-//   });
-// }
-
-export { changeBrToDiv }
-
-
-const keyupEvent = new Event('keyup');
-
 const changeBrToDiv = () => {
   // Finds all my 'areas' with Trix editors in my page
   let msgTrixInputDivs = document.querySelectorAll(".spkr-msg-form");
@@ -74,9 +8,8 @@ const changeBrToDiv = () => {
     let tlkBubbleStart = '<div class="tlk-bubble">';
     let tlkBubbleEnd = '</div>';
 
-
-    // $(`[target="${elemTarget}"]`).bind('input keyup', function(){
-    msgTrixInput.addEventListener('keyup', (event) => {
+    let elemTarget = msgTrixInput.getAttribute('target');
+    $(`[target="${elemTarget}"]`).bind('input keyup', function(){
 
       let textBlock = []
       let completeVar = true
@@ -90,6 +23,7 @@ const changeBrToDiv = () => {
       // handles the first line if there is one line
       let blockMatchDiv = msgTrixStore.value.match(/(?:<!--block-->)([\s\S]*?)(?=<\/div>)/);
 
+      // handles first word on mobile
       if(!blockMatchBr || !blockMatchDiv || !brMatchDiv || !brMatchesBr) {
         textBlock.push(tlkBubbleStart, msgTrixInput.value, tlkBubbleEnd);
         completeVar = false;
@@ -117,38 +51,16 @@ const changeBrToDiv = () => {
         }
       }
 
+      // see your result in the console
       console.log(textBlock.join(""))
       // joins the textBlock array and sets it to the value of the msgTrixStore
       msgTrixStore.value = textBlock.join("");
-      // alert(msgTrixInput.innerHTML);
     });
-
-
-    // let formId = div.getAttribute('target');
-    // $(`#${formId}`).submit( function(event) {
-    //     var form = this;
-    //     let finalInputValue = msgTrixInput.value
-    //     msgTrixInput.value = finalInputValue + " ";
-    //     msgTrixInput.dispatchEvent(keyupEvent);
-    //     console.log("DID A THING");
-    //     alert("HELO");
-    //     var document = element.editor.getDocument()
-    //     console.log(document.toString());
-
-    //     event.preventDefault();
-
-    //     setTimeout( function () {
-    //         form.submit();
-    //     }, 3000);
-    // });
-
   });
 }
 
-// export { triggerKeyupOnSubmit }
-
-// const triggerKeyupOnSubmit = () => {
-
-// }
+export { changeBrToDiv }
 
 
+// Non mobile line 12:
+// msgTrixInput.addEventListener('keyup', (event) => {
