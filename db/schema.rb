d3a16b26d87b82cd9e344f43b2150e3bfbbf2cac
Joshua Summers <ei.chler.summers@gmail.com>
2020-04-09 12:26:17 +0100

    change database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_04_03_130630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -44,9 +47,9 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   end
 
   create_table "draft_msgs", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "tlk_id", null: false
-    t.integer "spkr_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "tlk_id", null: false
+    t.bigint "spkr_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["spkr_id"], name: "index_draft_msgs_on_spkr_id"
@@ -66,9 +69,9 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   end
 
   create_table "msgs", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "tlk_id", null: false
-    t.integer "spkr_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "tlk_id", null: false
+    t.bigint "spkr_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "draft", default: false
@@ -88,8 +91,8 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   end
 
   create_table "spkrs", force: :cascade do |t|
-    t.integer "tlk_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "tlk_id", null: false
+    t.bigint "user_id", null: false
     t.string "name"
     t.string "bio"
     t.datetime "created_at", precision: 6, null: false
@@ -105,8 +108,8 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   end
 
   create_table "tlk_follows", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "tlk_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "tlk_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tlk_id"], name: "index_tlk_follows_on_tlk_id"
@@ -125,7 +128,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_130630) do
   end
 
   create_table "tlks", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
